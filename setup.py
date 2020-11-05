461df50ed9609db0e75f1a537e161aac1bea92fb
Kyle Fuller <kyle@fuller.li>
2020-11-05 19:00:09 +0000

    release: 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rivr-jwt',
-    version='0.1.0',
+    version='0.2.0',
     description='Making authentication with JWT (JSON Web Token) and rivr simple.',
     url='https://github.com/rivrproject/rivr-jwt',
     packages=find_packages(),
@@ -12,19 +12,18 @@ setup(
         'rivr_jwt': ['py.typed'],
     },
     install_requires=[
-        'rivr@git+https://github.com/rivrproject/rivr.git',
+        'rivr>=0.9.0',
         'PyJWT',
     ],
     author='Kyle Fuller',
     author_email='kyle@fuller.li',
     license='BSD',
     classifiers=(
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-      'Programming Language :: Python :: 3.8',
-      'Programming Language :: Python :: 3.9',
-      'License :: OSI Approved :: BSD License',
-    )
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License',
+    ),
 )
-
